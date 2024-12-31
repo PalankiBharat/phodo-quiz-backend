6a3f951b7bb2714417f665bf11bdd12d31ae35da
BharatKumar <bk9735732777@gmail.com>
2024-12-31 12:01:50 +0530

    Fixed Connection adn deployment

--- src/main/kotlin/com/iapprusher/application/di/AppModules.kt
+++ src/main/kotlin/com/iapprusher/application/di/AppModules.kt
@@ -7,6 +7,8 @@ import com.iapprusher.application.utils.StringConstants.QUESTIONS_COLLECTION_NAM
 import com.iapprusher.application.utils.StringConstants.TAG_COLLECTION_NAME
 import com.iapprusher.repo.QuestionRepo
 import com.iapprusher.repo.QuestionRepoImpl
+import com.iapprusher.repo.TagRepo
+import com.iapprusher.repo.TagRepoImpl
 import com.iapprusher.service.QuestionService
 import com.mongodb.client.model.IndexOptions
 import com.mongodb.client.model.Indexes
@@ -14,6 +16,7 @@ import com.mongodb.kotlin.client.coroutine.MongoCollection
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
 
 import io.ktor.server.application.*
+import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
@@ -25,10 +28,11 @@ fun Application.configureDI() {
         modules(
             module {
                 single { connectToMongoDB() }
-                single { provideQuestionCollection(get()) }
-                single { provideTagsCollection(get()) }
-                single { QuestionRepoImpl(get()) } bind QuestionRepo::class
+                single(named("question")) { provideQuestionCollection(get()) }
+                single(named("tag")) { provideTagsCollection(get()) }
+                single { QuestionRepoImpl(get((named("question")))) } bind QuestionRepo::class
                 single { QuestionService(get()) }
+                single { TagRepoImpl(get(named("tag"))) } bind TagRepo::class
 
             }
         )
--- src/main/kotlin/com/iapprusher/application/plugins/Databases.kt
+++ src/main/kotlin/com/iapprusher/application/plugins/Databases.kt
@@ -8,13 +8,15 @@ import io.ktor.server.config.*
 fun Application.connectToMongoDB(): MongoDatabase {
     val user = environment.config.tryGetString("db.mongo.user")
     val password = environment.config.tryGetString("db.mongo.password")
-    val host = environment.config.tryGetString("db.mongo.host") ?: "127.0.0.1"
+    val host = environment.config.tryGetString("db.mongo.host")
     //val port = environment.config.tryGetString("db.mongo.port") ?: "27017"
     val maxPoolSize = environment.config.tryGetString("db.mongo.maxPoolSize")?.toInt() ?: 20
     val databaseName = environment.config.tryGetString("db.mongo.database.name") ?: "myDatabase"
 
     val credentials = user?.let { userVal -> password?.let { passwordVal -> "$userVal:$passwordVal@" } }.orEmpty()
-    val uri = "mongodb://$credentials$host/?maxPoolSize=$maxPoolSize&w=majority"
+    println("Connecting to MongoDB at $host")
+    println("Connecting to MongoDB at $credentials")
+    val uri = "mongodb+srv://$credentials$host"
 
     val mongoClient = MongoClient.create(uri)
     val database = mongoClient.getDatabase(databaseName)
